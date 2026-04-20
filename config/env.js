0a43aa5c468a1496b23956e48d114c25858e4040
Celia <joyihirwecelia@gmail.com>
2026-04-20 07:43:49 +0200

    Library management system

--- config/env.js
+++ config/env.js
@@ -1,11 +1,7 @@
 import {config} from 'dotenv';
 const result = config({
-    path: `.env.${process.env.NODE_ENV || 'development'}.local`
+    path: `.env.${process.env.NODE_ENV || 'development'}`
 });
 
-console.log('ENV PATH:', `.env.${process.env.NODE_ENV || 'development'}.local`);
-console.log('DOTENV RESULT:', result);
-console.log("JWT_EXPIRES_IN: ", process.env.JWT_EXPIRES_IN);
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 export const {PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN} = process.env;
